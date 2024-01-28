037c63bf2170d0c492dabbe3f070f0544c1a44be
Priyanshu3023 <157918602+Priyanshu3023@users.noreply.github.com>
2024-01-29 00:38:08 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+function openNewPage() {
+    window.location.href = "newpage.html"; // Replace with the actual URL of your new page
+}
+
+function openRegisterPage() {
+    // Handle registration functionality or redirect to the registration page
+}
